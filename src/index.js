44229664a0007c76588e0a385e5ef5842459e89a
alan <alan.silva.cr41@gmail.com>
2024-05-10 12:09:05 -0300

    atualização 1.03

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import {con} from "./config/database.js";
+import routeProduto from "./routes/route.produtos.js";
+import routePedido from "./routes/route.pedidos.js";
 
 const app = express();
 
@@ -8,20 +9,8 @@ app.use(express.json());
 app.use(cors());
 
 //rotas
-app.get("/olamundo", function(req, res){
-    res.status(200).send("ola mundo ! Eu me chamo senhor pipoca ");
-});
-
-app.get("/produtos", function(req, res){
-        con.query('select * from produto', function(err, result){
-        if (err)
-            return res.status(500).send("ocorreu um erro: " + err.message);
-        else
-            return res.status(200).json(result);
-        })
-
-
-});
+app.use(routeProduto);
+app.use(routePedido);
 //levanta o servidor
 app.listen(3001, function(){
     console.log("Servidor executando na porta 3001");
