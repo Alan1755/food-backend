3ee9b7a5f1706659e2c2504b3c593933bae92de2
alan <alan.silva.cr41@gmail.com>
2024-05-22 12:00:36 -0300

    atualização 9

--- src/routes/route.produtos.js
+++ src/routes/route.produtos.js
@@ -1,17 +1,8 @@
 import { Router } from "express";
-import { con, query } from "../config/database.js";
+import ProdutoController from "../controllers/controller.produto.js";
 
 const routeProduto = Router();
 
-//rotas
-
-routeProduto.get("/produtos", function(req, res){
-        con.query('select * from produto', function(err, result){
-        if (err)
-            return res.status(500).send("ocorreu um erro: " + err.message);
-        else
-            return res.status(200).json(result);
-        })
-    });
+routeProduto.get("/produtos", ProdutoController.getAllProdutos);
 
 export default routeProduto;
