f9bb8c7cb3ff06b63b68d32725ae1f158f10def4
alan <alan.silva.cr41@gmail.com>
2024-06-05 10:19:31 -0300

    aleluia11.0

--- src/controllers/controller.usuario.js
+++ src/controllers/controller.usuario.js
@@ -1,3 +1,4 @@
+import { con } from "../config/database.js";
 import UsuarioModel from "../models/model.usuario.js";
 
 class UsuarioController {
@@ -22,6 +23,86 @@ class UsuarioController {
         }
     }
 
+    static createUsuario(req, res) {
+        const p = req.body;
+        const nome = p.nome;
+        const email = p.email;
+        const senha = p.senha;
+
+        try {
+            UsuarioModel.createUsuario(nome, email, senha, function(err, result) {
+                    if (err) {
+                        console.error("Erro ao cadastrar usuario: ", err);
+                        return res.status(500).json ({ error: "ocorreu um erro ao cadastrar o usuario. "});
+                    }
+
+                    return res.status(201).json( { message: "usuario inserido com sucesso.",
+                        data: {
+                            id: result.insertId,
+                            nome,
+                            email
+                        }
+                     })
+            });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json( {error: "Erro interno do servidor."  } );
+        }
+    }
+    static editUsuario(req, res){
+        const id = req.params.id;
+        const p = req.body;
+        const nome = p.nome;
+        const email = p.email;
+        
+        try {
+            UsuarioModel.editUsuario(id, nome, email, function(err, result){
+                    if (err){
+                        console.error("Erro ao editar o usuario: ", err);
+                        return res.status(500).json({ error: "Ocorreu um erro ao editar o usuario."});
+
+                    }
+
+                    if (result.affectedRows === 0){
+                        return res.status(404).json({ message: "Usuario nao foi encontrado"});
+
+                    } 
+                    return res.status(200).json({
+                        message: "Usuario editado com sucesso",
+                        data: {
+                            id, nome, email
+                        }
+                    })
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json ({ error: "erro interno no servidor"});
+        }
+        
+
+    }
+            static removeUsuario(req, res) {
+                let id = req.params.id;
+
+                try {
+                    UsuarioModel.removeUsuario(id, function(err, result){
+                        if (err) {
+                            console.error("Erro ao deletar usuario:", err);
+                            return res.status(500).json({ error: "Ocorreu um erro ao deletar o Usuario."} );
+                        
+                        }
+                        if (result.affectedRows ===0) {
+                            return res.status(404).json({ message: "Usuario não encontrado "})
+                            
+                        }
+                        return res.status(200).json({message: " Usuario deletado com sucesso ", data: {id} } );
+                    })
+                } catch (error) {
+                    console.error(error);
+                    res.status(500).json({error: "erro interno no servidor."});
+                }
+             }
 }
 
 export default UsuarioController;
--- src/models/model.usuario.js
+++ src/models/model.usuario.js
@@ -14,7 +14,42 @@ class UsuarioModel {
 
         });
     }
+  //metodo pra criar um novo usuario
+        static createUsuario(nome, email, senha, callback) {
+            let sql= `insert into usuario (nome, email, senha) values (?, ?, ?)`;
 
+        con.query(sql, [nome, email, senha], function(err, result) {
+            if (err)
+                callback(err, null);
+            else
+                callback(null, result);
+        });
+        }
+        // metodo para editar um usuario existente
+        static editUsuario(id, nome, email, callback) {
+            let sql= `update usuari set nome=?, email=? where id_usuario`;
+
+            con.query(sql, [nome, email, id], function(err, result) {
+                if (err)
+                    callback(err, null);
+                else
+                    callback(null, result)
+            });
+        }
+
+        // Método para remover um usuário
+        static removeUsuario(id, callback){
+            let sql = `delete from usuario where id_usuario=?`;
+
+                con.query(sql, [id], function(err, result){
+                    if (err) 
+                        callback(err, null)
+                    else
+                        callback(null, result)
+                        
+                    
+                });
+        }
 }
 
 export default UsuarioModel;
